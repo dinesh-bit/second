9e99a48c889b32527d2689d33e29c1df7459098c
dineshkumar bm <dineshkumar bm@LAPTOP-99V3NSHI>
2020-01-28 12:30:43 +0530

    final

---
+++ messenger/src/main/java/org/com/dinesh/messenger/service/MessageService.java
@@ -0,0 +1,73 @@
+ package org.com.dinesh.messenger.service;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+import org.com.dinesh.messenger.database.DatabaseClass;
+import org.com.dinesh.messenger.model.Message;
+
+public class MessageService {
+	
+	private Map<Long,Message> messages=DatabaseClass.getMessages();
+	public MessageService()
+	{
+		messages.put(1L,new Message(1, "hello word!", null, "dinesh"));
+		messages.put(2L,new Message(2, "hello jersey!", null, "dinesh"));
+	}
+ public List<Message> getAllMessages()
+   {
+	 
+	return new ArrayList<Message>(messages.values());	
+   }
+	public List<Message> getAllMessagesForYear(int year)
+	{
+		List<Message> messagesForYear=new ArrayList<>();
+		Calendar cal=Calendar.getInstance();
+		for(Message message:messages.values())
+		{
+			cal.setTime(message.getCreated());
+		
+				if(cal.get(Calendar.YEAR) == year)
+ {
+	 messagesForYear.add(message);
+	 
+ }
+			}
+			return messagesForYear;
+		}
+	
+		public List<Message> getAllMessagePaginated(int start,int size)
+		{
+			ArrayList<Message> list=new ArrayList<Message>(messages.values());
+		if(start +size>list.size())return new ArrayList<Message>();
+		return list.subList(start,start + size);
+		}
+	
+	
+	public Message getMessage(long Id)
+	{
+		return messages.get(Id);
+	}
+	 public Message addMessage(Message message)
+	 {
+		 message.setId(messages.size() +1);
+		 messages.put(message.getId(),message);
+		 return message;
+	 }
+	 public Message updateMessage(Message message)
+	 {
+		 if(message.getId()<=0)
+		 {
+		 return null;   
+	 }
+		 messages.put(message.getId(),message);
+		 return message;
+		 
+	 }
+	 
+	 public Message removeMessage(long Id)
+		{
+			return messages.remove(Id);
+		}
+}
+
